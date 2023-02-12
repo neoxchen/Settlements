c67ce15dadaeb2af245476dd532857b4faaf0695
Neo Chen <101343310+neoxchen@users.noreply.github.com>
2023-02-11 21:18:56 -0800

    Downgrade Java version to 17 cause paper doesn't support 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ description = "Enhanced villager mechanics"
 
 java {
     // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 dependencies {
@@ -46,7 +46,7 @@ tasks {
 
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(19)
+        options.release.set(17)
     }
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
